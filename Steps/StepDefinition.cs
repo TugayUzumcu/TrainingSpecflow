5e01b7e6a476e450153b4f6caaec1cc081058638
Tugay Uzinli <tugay.uzinli@capgemini.com>
2021-03-25 11:05:27 +0100

    antwoorden

--- Steps/StepDefinition.cs
+++ Steps/StepDefinition.cs
@@ -1,4 +1,5 @@
 ﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
 using SpecflowCursus.DriverGenerator;
 using SpecflowCursus.Pages;
 using System;
@@ -14,12 +15,44 @@ namespace SpecflowCursus.Steps
     {
         static IWebDriver driver = driverGenerator.getDriver();
         HomePage homePage = new HomePage(driver);
+        ReisplannerPage reisplannerPage = new ReisplannerPage(driver);
 
         [Given(@"de gebruiker is op de ns website")]
         public void GivenDeGebruikerIsOpDeNsWebsite()
         {
-            
+            homePage.getHeaderPlanJeReis();
         }
+        [When(@"gebruiker voert (.*) in")]
+        public void WhenGebruikerVoertIn(string location)
+        {
+            homePage.setStartPoint(location);
+            homePage.clickLocation();
+        }
+        [When(@"gebruiker vult zijn (.*) in")]
+        public void WhenGebruikerVultZijnIn(string destination)
+        {
+            homePage.setDestination(destination);
+            homePage.clickDestination();
+        }
+        [When(@"gebruiker klik op plannen om een reis uit te kiezen")]
+        public void WhenGebruikerKlikOpPlannenOmEenReisUitTeKiezen()
+        {
+            homePage.clickPlannen();
+        }
+        [Then(@"de gebruiker heeft de prijs van de reis gevonden")]
+        public void ThenDeGebruikerHeeftDePrijsVanDeReisGevonden()
+        {
+            reisplannerPage.getPrice();
+        }
+        [When(@"gebruiker vult in de (.*)")]
+        public void WhenGebruikerVultInDe(string date)
+        {
+            homePage.clickCalendar();
+            homePage.setData(date);
+        }
+
+
+
 
     }
 }
